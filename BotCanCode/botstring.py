f3b11a54e7bbccd7960c8d68532fc57b7f2734a3
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-04-29 01:07:50 +0200

    bot: add hardcoded strings and messages in a file

---
+++ BotCanCode/botstring.py
@@ -0,0 +1,3 @@
+WELCOME_DM = 'Bienvenue !\nRéponds moi avec une courte présentation de ta personne et de tes motivations et un organisateur te contactera !'
+BANNED = 'Vous n\'avez pas le droit de m\'écrire...'
+ALREADY_ACCEPTED = 'Cela ne sert plus à rien de me parler !'
