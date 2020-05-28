ce2829a88f4a04feb91e2b1271b38327583a2f3f
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-05-28 17:09:25 +0200

    bot: update conf and bot code

--- BotCanCode/bot.py
+++ BotCanCode/bot.py
@@ -1,9 +1,16 @@
+from discord.ext import commands
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from string import Template
 import conf
 import discord
 import os
+import smtplib
 import strings
 
-bot = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
 
 #
 #   Basics
@@ -12,16 +19,17 @@ bot = discord.Client()
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game('42'))
+    await bot.change_presence(activity=discord.Game('Learning Python'))
     print('>>> Bot is ready.')
 
 
 @bot.event
 async def on_member_join(member):
-    server = bot.get_guild(conf.SERVER_ID)
+    server = bot.get_guild(conf.SERVER)
     role_tmp = server.get_role(conf.ROLE_TMP)
-    print('>>> {member.nick} joined the server.')
+    print(f'>>> {member.nick} joined the server.')
     await member.add_roles(role_tmp)
+
 
 #
 #   Handling reactions on welcome message
@@ -33,10 +41,11 @@ async def on_raw_reaction_add(payload):
     channel = bot.get_channel(payload.channel_id)
     username = bot.get_user(payload.user_id)
     reaction = payload.emoji
-    if channel.id == conf.CHANNEL_ACCUEIL_ID:
-        print('>>> {username} added a {reaction.name} react.')
+    if channel.id == conf.CHANNEL_ACCUEIL:
+        print(f'>>> {username} added a {reaction.name} react.')
         if reaction.name == '\N{OK HAND SIGN}':
             await username.send(strings.WELCOME_DM)
+
 
 #
 #   Transfer DM presentation to correct private channel
@@ -45,31 +54,125 @@ async def on_raw_reaction_add(payload):
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user or message.guild is not None:
+    if message.author == bot.user:
         return
 
-    print('>>> {message.author} sent a message to the bot:\n"{message.content}".')
+    if message.guild is not None:
+        await bot.process_commands(message)
+        return
+	
+    print(f'>>> {message.author} sent a message to the bot:\n"{message.content}".')
 
-    server = bot.get_guild(conf.SERVER_ID)
+    server = bot.get_guild(conf.SERVER)
     role_participant = server.get_role(conf.ROLE_PARTICIPANT)
     role_organizer = server.get_role(conf.ROLE_ORGANIZER)
     member = server.get_member(message.author.id)
     if not member:
         await message.author.send(strings.BANNED)
+        await bot.process_commands(message)
         return
 
     if role_participant in member.roles or role_organizer in member.roles:
         await message.author.send(strings.ALREADY_ACCEPTED)
+        await bot.process_commands(message)
         return
 
     embed = discord.Embed(title='', colour=discord.Colour.green())
     embed.set_author(name=message.author)
     embed.add_field(name='Présentation', value=message.content, inline=False)
 
-    channel = bot.get_channel(conf.CHANNEL_PRESENTATIONS_ID)
+    channel = bot.get_channel(conf.CHANNEL_PRESENTATIONS)
     msg = await channel.send(embed=embed)
     await msg.add_reaction('\N{WHITE HEAVY CHECK MARK}')
     await msg.add_reaction('\N{CROSS MARK}')
+    await bot.process_commands(message)
+
+
+#
+#   Activity Mailing
+#
+
+
+def get_contacts(filename):
+    contacts = {}
+
+    try:
+        with open(filename) as f:
+            tmp_contacts = f.read().splitlines()
+            for contact in tmp_contacts:
+                name, email = contact.split(',')[0], contact.split(',')[1]
+                contacts[name] = email
+    except OSError as e:
+        print(f"bot-can-code: {e.strerror}", file=sys.stderr)
+
+    return contacts
+
+
+def read_template(filename):
+    try:
+        with open(filename) as template_file:
+            template_file_content = template_file.read()
+
+    except OSError as e:
+        print(f"bot-can-code: {e.strerror}", file=sys.stderr)
+
+    return Template(template_file_content)
+
+
+@bot.command()
+async def notif(ctx, activity, time):
+    print('>>> Entered notif command')
+
+    if ctx.channel.id != conf.CHANNEL_BOT:
+        print('>>> Invalid channel')
+        return
+
+    msg = f'Session "{activity}" prévue à {time}'
+
+    print(f'>>> Trying to send {msg}')
+
+    server = bot.get_guild(conf.SERVER)
+
+    # ping @Organizer on #staff
+
+    role_organizer = server.get_role(conf.ROLE_ORGANIZER)
+    channel = bot.get_channel(conf.CHANNEL_STAFF)
+    await channel.send(f'{role_organizer.mention} {msg}')
+
+
+    # ping @Partipant on #general
+
+    role_participant = server.get_role(conf.ROLE_PARTICIPANT)
+    channel = bot.get_channel(conf.CHANNEL_GENERAL)
+    await channel.send(f'{role_participant.mention} {msg}')
+
+
+    # Mailing
+    conn = smtplib.SMTP(host=conf.MAIL_HOST, port=conf.MAIL_PORT)
+    conn.starttls()
+    conn.login(conf.MAIL_ADDRESS, conf.MAIL_PASSWORD)
+
+    contacts = get_contacts('contacts.csv')
+    message_template = read_template('message.txt')
+
+    for name, email in contacts.items():
+        print(f'>>> Trying to email {name}:{email}')
+        msg = MIMEMultipart()
+        message = message_template.substitute(PERSON_NAME=name, ACTIVITY=activity, TIME=time)
+
+        msg['From'] = conf.MAIL_ADDRESS
+        msg['To'] = email
+        msg['Subject'] = f'[PROLOCADEMY] Session "{activity}" à {time}'
+
+        msg.attach(MIMEText(message, 'plain'))
+
+        conn.send_message(msg)
+        print('>>> msg should be sent')
+
+        del msg
+
+    conn.quit()
+
 
 #
 #   Main code for bot
