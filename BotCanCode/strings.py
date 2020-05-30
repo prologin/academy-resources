5ab648274574769fe19ac202507c901fe832fe86
Yves Stadler <yves.stadler@ocado.com>
2020-05-30 14:26:13 +0100

    Cleaning up bot - moved to another directory

--- BotCanCode/strings.py
+++
@@ -1,3 +0,0 @@
-WELCOME_DM = 'Bienvenue !\nRéponds moi avec une courte présentation de ta personne et de tes motivations.\n Un organisateur te contactera très prochainement!'
-BANNED = 'Vous n\'avez pas le droit de m\'écrire...'
-ALREADY_ACCEPTED = 'Ta demande est acceptée, cela ne sert plus à rien de me parler !'
